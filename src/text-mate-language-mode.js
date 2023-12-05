4504955ab61f65fd315ed9a2b5089aeee71675f9
Sebastian Balcerowiak <asiloisad.93@gmail.com>
2023-12-05 02:07:37 +0100

    Fix config name

--- src/text-mate-language-mode.js
+++ src/text-mate-language-mode.js
@@ -31,7 +31,7 @@ class TextMateLanguageMode {
     this.largeFileMode = params.largeFileMode;
     this.config = params.config;
     this.grammar = params.grammar || NullGrammar;
-    this.largeFileModeThreshold = atom.config.get('largeFileModeThreshold', {scope:this.grammar.scopeName})
+    this.largeFileModeThreshold = atom.config.get('editor.largeFileModeThreshold', {scope:this.grammar.scopeName})
     this.largeFileMode =
       params.largeFileMode != null
         ? params.largeFileMode
